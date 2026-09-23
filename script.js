23bd927bd3fdb9e690b14d92c0a436ed2099b22b
humayunk45423 <humayunk45423@gmail.com>
2026-09-23 14:52:54 +0600

    updating

--- script.js
+++ script.js
@@ -69,6 +69,25 @@
       }
     });
 
+    // Translate digits in all text nodes
+    const digitMap = lang === 'en' ? 
+      {'০':'0','১':'1','২':'2','৩':'3','৪':'4','৫':'5','৬':'6','৭':'7','৮':'8','৯':'9'} : 
+      {'0':'০','1':'১','2':'২','3':'৩','4':'৪','5':'৫','6':'৬','7':'৭','8':'৮','9':'۹','9':'৯'};
+
+    // Fix map definition
+    const map = lang === 'en' ? 
+      {'০':'0','১':'1','২':'2','৩':'3','৪':'4','৫':'5','৬':'6','৭':'7','৮':'8','৯':'9'} : 
+      {'0':'০','1':'১','2':'২','3':'৩','4':'৪','5':'৫','6':'৬','7':'৭','8':'৮','9':'৯'};
+
+    function convertDigits(node) {
+      if (node.nodeType === 3) {
+        node.nodeValue = node.nodeValue.replace(/[0-9০-৯]/g, match => map[match] || match);
+      } else if (node.nodeType === 1 && node.tagName !== 'SCRIPT' && node.tagName !== 'STYLE' && node.tagName !== 'INPUT') {
+        node.childNodes.forEach(convertDigits);
+      }
+    }
+    convertDigits(document.body);
+
     // Toggle visibility of lang-specific blocks if any
     document.querySelectorAll('.lang-block-bn').forEach((el) => {
       el.style.display = lang === 'bn' ? '' : 'none';
